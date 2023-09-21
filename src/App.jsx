f95f6bcf858c6e9435135a481b99445950147da9
Tomás Cárdenas <tomicardenas96@gmail.com>
2023-09-21 12:05:22 -0300

    Some general changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import "./App.css";
+import SideCard from "./components/SideCard";
+import Menu from "./components/menu";
 
 function App() {
   return (
     <>
-     
+      <SideCard />
+      <Menu />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
