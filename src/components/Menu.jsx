7862aa0022c2f55d5ab74b20eaa4168c9b680898
Tomás Cárdenas <tomicardenas96@gmail.com>
2023-09-21 02:22:10 -0300

    Created main menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+import '../style/Menu.css';
+
+function Menu() {
+    return (
+        <menu className='main-menu__container'>
+            <ul className='main-menu'>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>SKILLS</li>
+                <li>PORTFOLIO</li>
+                <li>CONTACT</li>
+            </ul>
+        </menu>
+    )
+}
+
+export default Menu;
