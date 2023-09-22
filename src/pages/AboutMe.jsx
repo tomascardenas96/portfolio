348c177a14fa9f908f59655c45e1645d48a2b1c9
Tomás Cárdenas <tomicardenas96@gmail.com>
2023-09-22 16:10:20 -0300

    Finished about me section, and some fixes in styles

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -7,8 +7,8 @@ function AboutMe() {
         <section className="about-me__container">
             <div className="about-me">
                 <BookMarker icon={<CiUser/>} title="About me"/>
-                <div>
-                    <h2>Every great design begin with an even Better story</h2>
+                <div className="about-me__content">
+                    <h2>Every great design begin with an even <span>Better story</span></h2>
                     <p>Since beginning my journey as a freelance designer nearly 6 years ago, I’ve done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I’m quietly confident, naturally curious, and perpetually working on improving my chopsone design problem at a time.</p>
                 </div>
             </div>
